1bd73ec534c6dd6c35b176178142245719769071
Mathew Morris-Parker <mathew@roseblade.media>
2026-03-18 16:16:50 +0000

    Corrected call to relation for FavIcon;

--- src/DataExtension/SiteTreeExtension.php
+++ src/DataExtension/SiteTreeExtension.php
@@ -178,7 +178,7 @@ class SiteTreeExtension extends \SilverStripe\Core\Extension
 	public function getIconFile(mixed $sizeH, mixed $sizeW)
 	{
 		$siteConfig = SiteConfig::current_site_config();
-		$icon 		= $siteConfig->FavIcon;
+		$icon 		= $siteConfig->FavIcon();
 		$function 	= $this->getOwner()->config()->icon_size_function;
 
 		/** Padding is the only one with 3 parameters (for the fill) */
